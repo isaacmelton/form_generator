14ceb11ba1a2a44169145c7f0cc3045336baa5bd
Brian L. McMichael <bmcmich2@my.westga.edu>
2015-06-10 21:10:29 -0700

    person model pre-migration

---
+++ FormsGenerator/app/views/people/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @person, :id, :first_name, :last_name, :email, :city, :state, :country, :sex, :created_at, :updated_at
--- FormsGenerator/config/routes.rb
+++ FormsGenerator/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :people
   resources :surveys
   resources :forms
   resources :answers
