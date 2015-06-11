32d4d1bc2e01ac2a586ec27b5da7e606cb7673b0
Brian L. McMichael <bmcmich2@my.westga.edu>
2015-06-10 21:04:44 -0700

    answer model post-migration

--- FormsGenerator/db/schema.rb
+++ FormsGenerator/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611040028) do
+ActiveRecord::Schema.define(version: 20150611040343) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string   "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string   "question"
