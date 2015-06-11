5d49acba140b4bf10c8887f23514e7d1e2431d7d
Brian L. McMichael <bmcmich2@my.westga.edu>
2015-06-10 21:08:06 -0700

    form model pre-migration

--- FormsGenerator/config/routes.rb
+++ FormsGenerator/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :forms
   resources :answers
   resources :questions
   # The priority is based upon order of creation: first created -> highest priority.
