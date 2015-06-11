1af675345b05f8e29539773db5c6e1eaa21d1e25
Brian L. McMichael <bmcmich2@my.westga.edu>
2015-06-10 21:11:11 -0700

    person survey form model post-migration

--- FormsGenerator/db/schema.rb
+++ FormsGenerator/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611040343) do
+ActiveRecord::Schema.define(version: 20150611041015) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "answer"
@@ -19,8 +19,30 @@ ActiveRecord::Schema.define(version: 20150611040343) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "forms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "people", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "sex"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string   "question"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "surveys", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
