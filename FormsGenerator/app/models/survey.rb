828045fc69e5fcb231454cf3c60e80950d903e9b
Isaac Melton <imelton1@my.westga.edu>
2015-06-16 08:18:24 -0400

    Question areas now add questions to the Db. Survey names now display. Empty questions will not be added to the DB

--- FormsGenerator/app/models/survey.rb
+++ FormsGenerator/app/models/survey.rb
@@ -1,5 +1,5 @@
 class Survey < ActiveRecord::Base
 	has_many :questions, :dependent => :destroy
-	accepts_nested_attributes_for :questions
+	accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:question].blank? }, :allow_destroy => true
 	## belongs_to :creator, :class_name => 'Person', :foreign_key => 'creator_id'
 end
