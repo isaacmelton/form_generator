35d8a1307f1c4b052d8663bec18793e716ca2fb6
Brian McMichael <brian@brianmcmichael.com>
2015-06-28 15:30:17 -0400

    Add Header

--- FormsGeneratorPHP/view/footer.php
+++ FormsGeneratorPHP/view/footer.php
@@ -1,8 +1,8 @@
-<div id="footer">
-    <p class="copyright">
-        &copy; <?php echo date("Y"); ?> Brian McMichael, Isaac Melton, Forrest Wilbur.
-    </p>
-</div>
-</div><!-- end page -->
-</body>
+            <div id="footer">
+                <p class="copyright">
+                    &copy; <?php echo date("Y"); ?> Brian McMichael, Isaac Melton, Forrest Wilbur.
+                </p>
+            </div>
+        </div><!-- end page -->
+    </body>
 </html>
---
+++ FormsGeneratorPHP/view/header.php
@@ -0,0 +1,31 @@
+<?php
+// determine the absolute path to the style sheet main.css
+$uri = $_SERVER['REQUEST_URI'];
+$dirs = explode('/', $uri);
+$i = 1;
+$path = '/';
+while ($dirs[$i] != "FormsGeneratorPHP") {
+    $path .= $dirs[$i] . '/';
+    $i += 1;
+}
+$path .= 'FormsGeneratorPHP/';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- the head section -->
+<head>
+    <title>Forms Generator</title>
+    <link rel="stylesheet" type="text/css"
+          href="<?php echo $path . 'main.css'; ?>" />
+</head>
+
+<!-- the body section -->
+<body>
+<div id="page">
+    <div id="header">
+        <h1>Forms Generator</h1>
+        <p>Interactive Forms Generation</p>
+        <ul class="nav"><li><a href="http://<?php echo $_SERVER['HTTP_HOST'].$path; ?>">Home</a></li></ul>
+    </div>
+   
