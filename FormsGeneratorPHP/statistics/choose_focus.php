c3f172f96092d3e1a9d1d2f341c4e055e4260c9d
Forrest Wilbur <fwilbur1@my.westga.edu>
2015-06-30 11:22:23 -0500

    Still having issues with certain files not tracking when I commit.  Recommitting.

--- FormsGeneratorPHP/statistics/choose_focus.php
+++ FormsGeneratorPHP/statistics/choose_focus.php
@@ -8,7 +8,7 @@
 
 <?php 
     // $takers will wait until users are added to the DB
-    if (!isset($surveys)) || (!isset($authors)) || /* (!isset($takers)) */ { 
+    if (!isset($surveys)) || (!isset($authors)) /* || (!isset($takers)) */ { 
         header("Location: index.php"); 
     } 
 ?>
@@ -16,3 +16,31 @@
 <p>
     Hey look!  This is where you choose either the survey or author (or ultimately survey-taker) you want to inspect statistics-wise.
 </p>
+
+<form action="surveys" method="post" id="aligned">
+    <select name="id">
+        <?php foreach ($surveys as $survey): ?>
+            <option value="<?php echo $survey['id']; ?>">
+                <?php echo $survey['name']; ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+    <br />
+    <input type="submit" value="Inspect" />
+</form>
+
+<br />
+
+<form action="authors" method="post" id="aligned">
+    <select name="id">
+        <?php foreach ($authors as $author): ?>
+            <option value="<?php echo $author['id']; ?>">
+                <?php echo $author['email']; ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+    <br />
+    <input type="submit" value="Inspect" />
+</form>
+
+<br />
