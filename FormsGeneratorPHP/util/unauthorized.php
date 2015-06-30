df59151aa48526704b4696ea0b00d6fb6904a8d4
Brian McMichael <brian@brianmcmichael.com>
2015-06-29 22:21:49 -0400

    Add utilities

--- FormsGeneratorPHP/util/unauthorized.php
+++ FormsGeneratorPHP/util/unauthorized.php
@@ -1,7 +1,9 @@
-<div id="main">
-    <h2>Unauthorized User</h2>
 
-    <p>You are not authorized to view this page</p>
+
+<div id="main">
+	<h2>Unauthorized User</h2>
+
+	<p>You are not authorized to view this page</p>
 </div>
 
 <?php include('..\view\footer.php'); ?>
