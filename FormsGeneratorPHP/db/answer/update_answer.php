d6fce95f74caafd539f2c98ff630c0399290a9f4
Brian McMichael <brian@brianmcmichael.com>
2015-06-30 01:07:47 -0400

    update_answer.php works with PDO

--- FormsGeneratorPHP/db/answer/update_answer.php
+++ FormsGeneratorPHP/db/answer/update_answer.php
@@ -1,22 +1,24 @@
-<?php 
-include('config.php'); 
+<?php
 if (isset($_GET['id']) ) { 
 $id = (int) $_GET['id']; 
-if (isset($_POST['submitted'])) { 
-foreach($_POST AS $key => $value) { $_POST[$key] = mysql_real_escape_string($value); } 
-$sql = "UPDATE `answers` SET  `answer` =  '{$_POST['answer']}' ,  `created_at` =  '{$_POST['created_at']}' ,  `updated_at` =  '{$_POST['updated_at']}' ,  `question_id` =  '{$_POST['question_id']}'   WHERE `id` = '$id' "; 
-mysql_query($sql) or die(mysql_error()); 
-echo (mysql_affected_rows()) ? "Edited row.<br />" : "Nothing changed. <br />"; 
+if (isset($_POST['submitted'])) {
+
+$sql = $db->prepare("UPDATE `answers` SET  `answer` =  '{$_POST['answer']}' ,  `created_at` =  '{$_POST['created_at']}' ,  `updated_at` =  '{$_POST['updated_at']}' ,  `question_id` =  '{$_POST['question_id']}'   WHERE `id` = '$id' ");
+$count = $sql->execute() or die(print_r($sql->errorInfo()));
+
+echo ($count > 0) ? "Edited row.<br />" : "Nothing changed. <br />";
 echo "<a href='read_answer.php'>Back To Listing</a>"; 
-} 
-$row = mysql_fetch_array ( mysql_query("SELECT * FROM `answers` WHERE `id` = '$id' ")); 
-?>
+}
+
+$result = $db->query("SELECT * FROM `answers` WHERE `id` = '$id' ");
+    $row = $result->fetch(PDO::FETCH_ASSOC);
+    ?>
 
 <form action='' method='POST'> 
-<p><b>Answer:</b><br /><input type='text' name='answer' value='<?= stripslashes($row['answer']) ?>' /> 
-<p><b>Created At:</b><br /><input type='text' name='created_at' value='<?= stripslashes($row['created_at']) ?>' /> 
-<p><b>Updated At:</b><br /><input type='text' name='updated_at' value='<?= stripslashes($row['updated_at']) ?>' /> 
-<p><b>Question Id:</b><br /><input type='text' name='question_id' value='<?= stripslashes($row['question_id']) ?>' /> 
+<p><b>Answer:</b><br /><input type='text' name='answer' value='<?php echo stripslashes($row['answer']) ?>' />
+<p><b>Created At:</b><br /><input type='text' name='created_at' value='<?php echo stripslashes($row['created_at']) ?>' />
+<p><b>Updated At:</b><br /><input type='text' name='updated_at' value='<?php echo stripslashes($row['updated_at']) ?>' />
+<p><b>Question Id:</b><br /><input type='text' name='question_id' value='<?php echo stripslashes($row['question_id']) ?>' />
 <p><input type='submit' value='Edit Row' /><input type='hidden' value='1' name='submitted' /> 
 </form> 
-<? } ?> 
+<?php } ?>
