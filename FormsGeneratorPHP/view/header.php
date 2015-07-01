e903ea3c15c1d46441160fdc8e456ae195aec6eb
imelton <imelton1@my.westga.edu>
2015-06-30 22:26:09 -0400

    Added Bootstrap to the navbar at the top per issue #12

--- FormsGeneratorPHP/view/header.php
+++ FormsGeneratorPHP/view/header.php
@@ -18,21 +18,55 @@ $path .= '';
 <!-- the head section -->
 <head>
     <title>Forms Generator</title>
-    <link rel="stylesheet" type="text/css"
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+	<link rel="stylesheet" type="text/css"
           href="<?php echo $path . 'css/main.css'; ?>" />
+		  
 </head>
 
 <!-- the body section -->
 <body>
 <div id="page">
-    <div id="header">
-        <h1>Forms Generator</h1>
-        <p>Interactive Forms Generation</p>
-        <ul class="nav"><li><a href="http://<?php echo $_SERVER['HTTP_HOST'].$path; ?>">Home</a></li></ul>
-        <ul class="nav"><li><a href="http://<?php echo $_SERVER['HTTP_HOST'].$path; ?>">Link 2</a></li></ul>
-        <ul class="nav"><li><a href="http://<?php echo $_SERVER['HTTP_HOST'].$path; ?>">Link 3</a></li></ul>
-        <ul class="nav"><li><a href="http://<?php echo $_SERVER['HTTP_HOST'].$path; ?>">Link 4</a></li></ul>
+    <header class="navbar-inverse" role="banner">
+	<nav class="navbar navbar-inverse">
+		<div class="container">
+
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Forms Generator</a>
     </div>
 
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="http://<?php echo $_SERVER['HTTP_HOST'].$path; ?>">Home <span class="sr-only">(current)</span></a></li>
+		<li class="active"><a href="http://<?php echo $_SERVER['HTTP_HOST'].$path; ?>">Link 2<span class="sr-only">(current)</span></a></li>
+	    <li class="active"><a href="http://<?php echo $_SERVER['HTTP_HOST'].$path; ?>">Link 3<span class="sr-only">(current)</span></a></li>
+		<li class="active"><a href="http://<?php echo $_SERVER['HTTP_HOST'].$path; ?>">Link 4<span class="sr-only">(current)</span></a></li>
+
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</div>	
+  </nav>
+
+</header>
+        <p>Interactive Forms Generation</p>
+        <p>Enter links/buttons to create survey/take survey, etc here.</p>
+    </div>
+
+</div>
 
    
