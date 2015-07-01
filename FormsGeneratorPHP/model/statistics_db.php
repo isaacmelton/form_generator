f8eabe0ec56b059c8b0541950209cacda21b15ed
Forrest Wilbur <fwilbur1@my.westga.edu>
2015-07-01 09:56:50 -0500

    Progress on the model stuffs for statistics.  Now need to implement it, which will be a little trickier.  Apologies in advance for somewhat hack-ish code.

--- FormsGeneratorPHP/model/statistics_db.php
+++ FormsGeneratorPHP/model/statistics_db.php
@@ -36,4 +36,84 @@ function get_authors() {
     }
 }
 
+function get_question_stats_by_survey_id($survey_id) {
+    global $db;
+    $query =
+    "SELECT surveys.title AS title,
+    CONCAT(people.first_name, ' ', people.last_name) AS name,
+    people.email AS email,
+    questions.question AS question,
+    answers.answer AS answer, 
+    COUNT(answer_id) AS choice_count,
+    tottable.total AS total
+    FROM recorded_answers
+    INNER JOIN answers
+    ON answer_id = answers.id
+    LEFT JOIN (SELECT answers.question_id AS question_id,
+               COUNT(answer_id) AS total
+               FROM recorded_answers
+               INNER JOIN answers
+               ON answer_id = answers.id
+               GROUP BY answers.question_id) AS tottable
+    ON answers.question_id = tottable.question_id
+    INNER JOIN questions
+    ON answers.question_id = questions.id
+    INNER JOIN surveys
+    ON questions.survey_id = surveys.id
+    INNER JOIN people
+    ON surveys.person_id = people.id
+    WHERE surveys.id = :survey_id
+    GROUP BY answer_id, answers.question_id"
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':survey_id', $survey_id);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $statement->closeCursor();
+        return $result;
+    } catch (PDOException $e) {
+        display_db_error($e->getMessage());
+    }
+}
+
+function get_question_stats_by_author_id($author_id) {
+    global $db;
+    $query =
+    "SELECT surveys.title AS title,
+    CONCAT(people.first_name, ' ', people.last_name) AS name,
+    people.email AS email,
+    questions.question AS question,
+    answers.answer AS answer, 
+    COUNT(answer_id) AS choice_count,
+    tottable.total AS total
+    FROM recorded_answers
+    INNER JOIN answers
+    ON answer_id = answers.id
+    LEFT JOIN (SELECT answers.question_id AS question_id,
+               COUNT(answer_id) AS total
+               FROM recorded_answers
+               INNER JOIN answers
+               ON answer_id = answers.id
+               GROUP BY answers.question_id) AS tottable
+    ON answers.question_id = tottable.question_id
+    INNER JOIN questions
+    ON answers.question_id = questions.id
+    INNER JOIN surveys
+    ON questions.survey_id = surveys.id
+    INNER JOIN people
+    ON surveys.person_id = people.id
+    WHERE people.id = :author_id
+    GROUP BY answer_id, answers.question_id"
+    try {
+        $statement = $db->prepare($query);
+        $statement->bindValue(':author_id', $author_id);
+        $statement->execute();
+        $result = $statement->fetchAll();
+        $statement->closeCursor();
+        return $result;
+    } catch (PDOException $e) {
+        display_db_error($e->getMessage());
+    }
+}
+
 ?>
