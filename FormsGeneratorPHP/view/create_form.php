031e5dbfcbeb30c0cec3a291377a0c68a4d4cd70
Brian McMichael <brian@brianmcmichael.com>
2015-07-05 00:12:16 -0400

    Working on the create form. Skeletoned out the workflow. I need to javascript the cloner and probably add a counter because we're going to need an array within an array.

--- FormsGeneratorPHP/view/create_form.php
+++ FormsGeneratorPHP/view/create_form.php
@@ -3,43 +3,73 @@
 // Ref: http://stackoverflow.com/questions/29225320/saving-multiple-form-data-to-mysql-using-php
 // Ref: http://stackoverflow.com/questions/18156505/insert-multiple-fields-using-foreach-loop
 
-if(isset($_POST['submit'])){
+if(isset($_POST['submit'])) {
 
     $surveyTitle = $_POST['surveyTitle'];
-    $row_data = array();
-    foreach($_POST['question'] as $row=>$question){
-        $question = mysql_real_escape_string($question);
-        $answer= mysql_real_escape_string($_POST['answer'][$row]);
+    $questions = $_POST['question'];
+    $answers = $_POST['answer'];
 
-        $row_data[] = "('$question','$answer')";
+    //TODO Create survey
+    $sql = "INSERT INTO survey
+            VALUES ()";  //TODO
+    if ($db->query($sql)) {
+        echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+    } else {
+        echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+    }
+
+    $db = null;
+
+    //TODO iterate over questions and add
+    foreach ($questions as $question) {
+        $sql = "INSERT INTO question
+                VALUES ()";  //TODO
+        if ($db->query($sql)) {
+            echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+        } else {
+            echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+        }
+        //TODO iterate over answers and add
+        foreach ($answers as $answers) {
+            $sql = "INSERT INTO answers
+                    VALUES ()";  //TODO
+            if ($db->query($sql)) {
+                echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+            } else {
+                echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+            }
+        }
+    }
+
+    try {
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    } catch (PDOException $e) {
+        echo $e->getMessage();
     }
 }
-if(!empty($row_data)){
-
-    //$insert_query = mysql_query("INSERT INTO surveys( " DATA "  ) VALUES".implode(',', $row_data));
-
-    if(!$insert_query){
-        echo "Error: " . mysql_error();
-    }else{
-        echo "Data Saved Successfully";
-    }
-}
-
 ?>
-
-<h1>This needs to be more fixed and allow for multiple answers.</h1>
 
 <form name="createForm" id="createForm" action="create_form" method="post">
 
-    Survey Name: <input type="text" name="surveyTitle">
-    <table class="itemTable">
-    <tr class="cloneable">
-        <td>Question<input type="text" name="question[]"></td>
-        <td>Answer<input type="text" name="answer[]"></td>
-    </tr>
-    </tbody>
+
+    Survey Name: <input type="text" name="surveyTitle"><br>
+
+    <table class="question_table">
+        <tr></tr>
+
     </table>
-    </div>
+    <table class="itemTable">
+    <input class="cloneable">
+        <tr>
+            <td>Question<input type="text" name="question[]"></td>
+            <td><button id="add_question">+</button></td>
+
+            <td>Answer<input type="text" name="answer[]"></td>
+            <td><button id="add_answer">+</button></td>
+        </tr>
+    </table>
     <div class="eventButtons">
         <input type="submit" name="submit" id="submit" value="Save">
         <input type="reset" name="reset" id="reset" value="Clear"  class="btn">
