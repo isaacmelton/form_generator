1416f4c667507c034673d30293cc50400cfa7cc0
Brian McMichael <brian@brianmcmichael.com>
2015-07-06 19:44:58 -0400

    Submit questions and answers to the db.

--- FormsGeneratorPHP/view/create_form.php
+++ FormsGeneratorPHP/view/create_form.php
@@ -6,38 +6,44 @@
 if(isset($_POST['submit'])) {
 
     $surveyTitle = $_POST['survey_title'];
-    //$questions = $_POST['question'];
-    //$answers = $_POST['answer'];
-    $now = new DateTime('now');
+    $questions = $_POST['question'];
+    $answers = $_POST['answer'];
+    $now = date("Y-m-d H:i:s");
 
     //TODO Create survey
     $sql = "INSERT INTO surveys (person_id, title, active)
             VALUES ('1','".$_POST['survey_title']."','1')";  //TODO add user
     if ($db->query($sql)) {
-        echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+        $survey_id = $db->lastInsertId();
 
-/*
-        //TODO iterate over questions and add
-        foreach ($questions as $question) {
-            $sql = "INSERT INTO question
-                VALUES ()";  //TODO
-            if ($db->query($sql)) {
-                echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
-            } else {
-                echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
-            }
-            //TODO iterate over answers and add
-            foreach ($answers as $answers) {
-                $sql = "INSERT INTO answers
-                    VALUES ()";  //TODO
+        if (isset($questions)) {
+            $sequence = 1;
+            foreach ($questions as $question) {
+
+                $sql = "INSERT INTO questions (sequence, question, survey_id, created_at)
+                VALUES ('".$sequence."', '".$question."', '".$survey_id."', '".$now."')";  //TODO
                 if ($db->query($sql)) {
-                    echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+                    $question_id = $db->lastInsertId();
+
+                    if (isset($answers[($sequence-1)])) {
+
+                        $answer_sequence = 1;
+                        foreach ($answers[$sequence-1] as $answer) {
+                            $sql = "INSERT INTO answers (sequence, answer, created_at, question_id)
+                            VALUES ('".$answer_sequence."','".$answer."','".$now."','".$question_id."')";  //TODO
+                            if ($db->query($sql)) {
+                            } else {
+                                echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+                            }
+                            $answer_sequence++;
+                        }
+                    }
                 } else {
                     echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
                 }
+                $sequence++;
             }
         }
-  */
     } else {
         echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
     }
