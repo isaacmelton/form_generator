353fef2f9901b3b6bb590bf2240739dede88b34c
Brian McMichael <brian@brianmcmichael.com>
2015-07-13 22:44:03 -0400

    Survey data now submits to 'recorded_answers' in db.

--- db/take_survey_db.php
+++ db/take_survey_db.php
@@ -13,47 +13,27 @@ if(isset($_POST['submit'])) {
     */
 
     $survey_id = $_POST['survey'];
-    $questions = get_questions($survey_id);
-    $answers = get_answers();
+    $answers = get_answers_by_id($survey_id);
+    $questions = get_question_ids_per_survey($survey_id);
     $now = date("Y-m-d H:i:s");
 
-    $sql = "INSERT INTO recorded_answers "
-    //TODO Create survey
-    $sql = "INSERT INTO surveys (person_id, title, active)
-            VALUES ('1','".$_POST['survey_title']."','1')";  //TODO add user
-    if ($db->query($sql)) {
-        $survey_id = $db->lastInsertId();
+    echo print_r(get_question_ids_per_survey($survey_id));
 
-        if (isset($questions)) {
-            $sequence = 1;
-            foreach ($questions as $question) {
+    foreach ($questions as $question) {
+        $question_tag = "question_".$question['id'];
+        //echo $question_tag;
+        //echo $_POST[$question_tag];
+        if (isset($_POST[$question_tag])) {
+            //TODO add user id
+            $sql = "INSERT INTO recorded_answers ( user_id, answer_id, survey_id, created_at )
+            VALUES ('1','".str_replace('question_', '', $_POST[$question_tag])."','".$survey_id."', '".$now."')";
 
-                $sql = "INSERT INTO questions (sequence, question, survey_id, created_at)
-                        VALUES ('".$sequence."', '".$question."', '".$survey_id."', '".$now."')";  //TODO
-                if ($db->query($sql)) {
-                    $question_id = $db->lastInsertId();
-
-                    if (isset($answers[($sequence-1)])) {
-
-                        $answer_sequence = 1;
-                        foreach ($answers[($sequence-1)] as $answer) {
-                            $sql = "INSERT INTO answers (sequence, answer, created_at, question_id)
-                            VALUES ('".$answer_sequence."','".$answer."','".$now."','".$question_id."')";  //TODO
-                            if ($db->query($sql)) {
-                            } else {
-                                echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
-                            }
-                            $answer_sequence++;
-                        }
-                    }
-                } else {
-                    echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
-                }
-                $sequence++;
+            if ($db->query($sql)) {
+                $survey_result_id = $db->lastInsertId();
+            } else {
+                echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
             }
         }
-    } else {
-        echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
     }
 
     try {
@@ -61,8 +41,6 @@ if(isset($_POST['submit'])) {
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
-
-    */
 
 }
 ?>
