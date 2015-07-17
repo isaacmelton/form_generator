a1a6fe1c04f488912fb65fcb553f13db353d40c4
Brian McMichael <brian@brianmcmichael.com>
2015-07-17 01:19:47 -0400

    Update create_form_db to use prepared statements.

--- db/create_form_db.php
+++ db/create_form_db.php
@@ -8,44 +8,58 @@ if(isset($_POST['submit'])) {
     $now = date("Y-m-d H:i:s");
 	$person_id = $_POST['person_id'];
 
-
-
-    //TODO Create survey
-    $sql = "INSERT INTO surveys (person_id, title, active)
-            VALUES ('".$_POST['person_id']."','".$_POST['survey_title']."','1')";  //TODO add user
-    if ($db->query($sql)) {
+    $query = "INSERT INTO surveys (person_id, title, active, created_at)
+              VALUES (:person_id, :survey_title, '1', :now)";
+    $statement = $db->prepare($query);
+    $statement->bindValue(':person_id', $_POST['person_id']);
+    $statement->bindValue(':survey_title', $_POST['survey_title']);
+    $statement->bindValue(':now', $now);
+    if($statement->execute()) {
         $survey_id = $db->lastInsertId();
+        $statement->closeCursor();
 
         if (isset($questions)) {
             $sequence = 1;
             foreach ($questions as $question) {
 
-                $sql = "INSERT INTO questions (sequence, question, survey_id, created_at)
-                        VALUES ('".$sequence."', '".$question."', '".$survey_id."', '".$now."')";  //TODO
-                if ($db->query($sql)) {
+                $query = "INSERT INTO questions (sequence, question, survey_id, created_at)
+                          VALUES (:sequence, :question, :survey_id, :now)";
+                $statement = $db->prepare($query);
+                $statement->bindValue(':sequence', $sequence);
+                $statement->bindValue(':question', $question);
+                $statement->bindValue(':survey_id', $survey_id);
+                $statement->bindValue(':now', $now);
+                if ($statement->execute()) {
                     $question_id = $db->lastInsertId();
+                    $statement->closeCursor();
 
                     if (isset($answers[($sequence-1)])) {
-
                         $answer_sequence = 1;
                         foreach ($answers[($sequence-1)] as $answer) {
-                            $sql = "INSERT INTO answers (sequence, answer, created_at, question_id)
-                            VALUES ('".$answer_sequence."','".$answer."','".$now."','".$question_id."')";  //TODO
-                            if ($db->query($sql)) {
+
+                            $query = "INSERT INTO answers(sequence, answer, created_at, question_id)
+                                      VALUES (:answer_sequence, :answer, :now, :question_id)";
+                            $statement = $db->prepare($query);
+                            $statement->bindValue(':answer_sequence', $answer_sequence);
+                            $statement->bindValue(':answer', $answer);
+                            $statement->bindValue(':now', $now);
+                            $statement->bindValue(':question_id', $question_id);
+                            if ($statement->execute()) {
+                                $statement->closeCursor();
                             } else {
-                                echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+                                echo "<script type= 'text/javascript'>alert('Answers not successfully Inserted.');</script>";
                             }
                             $answer_sequence++;
                         }
                     }
                 } else {
-                    echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+                    echo "<script type= 'text/javascript'>alert('Questions not successfully Inserted.');</script>";
                 }
                 $sequence++;
             }
         }
     } else {
-        echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+        echo "<script type= 'text/javascript'>alert('Survey not successfully Inserted.');</script>";
     }
 
     try {
