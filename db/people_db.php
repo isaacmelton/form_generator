df3df87135c359f7b0dad74a6e0037bf0a8cd3e7
Brian McMichael <brian@brianmcmichael.com>
2015-07-17 23:48:53 -0400

    Update answer db

--- db/people_db.php
+++ db/people_db.php
@@ -30,7 +30,7 @@ function add_person($first_name, $last_name, $email, $city, $state, $country, $s
             (first_name, last_name, email, city, state, country, sex)
         VALUES (
                '$first_name', '$last_name', '$email',
-               '$city', '$state', '$country', '$sex')";s
+               '$city', '$state', '$country', '$sex')";
     $db->exec($query);
 }
 */
