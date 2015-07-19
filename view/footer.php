fa90aca4321617e7521f9247716edaa085dd0cd6
Brian McMichael <brian@brianmcmichael.com>
2015-07-19 15:29:46 -0400

    Add sticky footer.

--- view/footer.php
+++ view/footer.php
@@ -1,15 +1,13 @@
-<div class="container-fluid">
 
-    <div class="col-md-3"></div>
-    <div class="col-md-5 text-center" id="footer">
-
-        <p class="copyright">
-            &copy; <?php echo date("Y"); ?> Brian McMichael, Isaac Melton, Forrest Wilbur.
-        </p>
-
-
+<div class="footer">
+    <div class="container">
+        <div class="col-md-3"></div>
+        <h4>&copy; <?php echo date("Y"); ?> Brian McMichael, Isaac Melton, Forrest Wilbur.</h4>
     </div>
-</div><!-- end page -->
+</div>
+
+
+    <!-- end page -->
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </body>
