bcb02c2745295de3a7361f11160998bfc69deabe
Brian McMichael <brian@brianmcmichael.com>
2015-07-19 13:00:47 -0400

    Fix main link

--- view/header.php
+++ view/header.php
@@ -59,7 +59,7 @@ $path .= '';
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="#">Forms Generator</a>
+                        <a class="navbar-brand" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>">Forms Generator</a>
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
