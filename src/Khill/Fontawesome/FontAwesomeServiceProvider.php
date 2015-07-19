60c8dcfb8344e5b82eb0bee147d3955ee75a8d99
Brian McMichael <brian@brianmcmichael.com>
2015-07-19 15:32:10 -0400

    Add FontAwesome PHP

---
+++ src/Khill/Fontawesome/FontAwesomeServiceProvider.php
@@ -0,0 +1,36 @@
+<?php namespace Khill\Fontawesome;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\AliasLoader;
+
+class FontAwesomeServiceProvider extends ServiceProvider
+{
+    protected $defer = false;
+
+    public function boot()
+    {
+        $this->package('khill/fontawesome');
+
+        include __DIR__.'/../../routes.php';
+    }
+
+    public function register()
+    {
+        $this->app['fontawesome'] = $this->app->share(function($app)
+        {
+            return new FontAwesome();
+        });
+
+        $this->app->booting(function()
+        {
+            $loader = AliasLoader::getInstance();
+            $loader->alias('FA', 'Khill\Fontawesome\FontAwesomeFacade');
+        });
+    }
+
+    public function provides()
+    {
+        return array('fontawesome');
+    }
+
+}
---
+++ src/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('fontawesome', function() {
+    return View::make('fontawesome::home');
+});
